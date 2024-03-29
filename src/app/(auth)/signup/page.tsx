b955426e1ba432fce8a0322be35dc4a32d849bb5
Olorunnishola Olamilekan <olorunnisholaolamilekan@gmail.com>
2024-03-29 16:52:54 +0100

    style: completed mobile layout for signup  page

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,9 +1,48 @@
+import Input from '@/components/Input'
+import Link from 'next/link'
+import Button from '../Button'
+
+import '@/sass/pages/signup.scss'
+
 interface SignupProps {}
 
 const Signup = function ({}: SignupProps) {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="signup">
+      <p className="signup__header">Hello bookworm</p>
+      <p className="signup__subheader">
+        Sign up here to get wonderful deals on your book purchases
+      </p>
+      <form action="">
+        <Input
+          htmlFor="name"
+          label="name"
+          type="text"
+          placeholder="enter your name"
+        />
+        <Input
+          htmlFor="email"
+          label="email"
+          type="email"
+          placeholder="enter your email"
+        />
+        <Input
+          htmlFor="password"
+          label="password"
+          type="password"
+          placeholder="enter your password"
+        />
+        <Input
+          htmlFor="passwordConfirm"
+          label="confirm password"
+          type="password"
+          placeholder="confirm your password"
+        />
+        <Button>sign up</Button>
+      </form>
+      <p className="signup__login">
+        Already have an account? <Link href="/login">Sign in</Link>{' '}
+      </p>
     </div>
   )
 }
